15d57223fb3e5babf022512e680c14efe01653de
ramesh22blr <blr22ramesh@gmail.com>
2023-05-29 19:41:30 +0530

    wrker node

---
+++ eklctrl/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Include the VPC peering configuration file
+module "vpc_peering" {
+  source = "./eksvpcpeering"
+}
--- eklctrl/workernodevpc.tf
+++ eklctrl/workernodevpc.tf
@@ -91,16 +91,4 @@ resource "aws_network_acl_association" "worker_nacl_association_b" {
 }
 
 
-# Define VPC peering connection
-resource "aws_vpc_peering_connection" "vpc_peering" {
-  peer_vpc_id   = aws_vpc.worker_vpc.id        # Worker node VPC ID
-  vpc_id        = aws_vpc.eks_vpc.id       # Control plane VPC ID
-  auto_accept   = true
-}
 
-# Accept VPC peering connection from the worker VPC side
-resource "aws_vpc_peering_connection_accepter" "vpc_peering_accepter" {
-  provider      = aws.us-east-1
-  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
-  auto_accept   = true
-}
