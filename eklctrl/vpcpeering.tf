31639278cde099b1ab48438bb0e9481f0c5d9545
ramesh22blr <blr22ramesh@gmail.com>
2023-05-29 21:06:19 +0530

    wrker node

--- eklctrl/vpcpeering.tf
+++ eklctrl/vpcpeering.tf
@@ -3,6 +3,5 @@
 resource "aws_vpc_peering_connection" "peering" {
   vpc_id               = aws_vpc.eks_vpc.id
   peer_vpc_id          = aws_vpc.worker_vpc.id
-  peer_region          = "us-east-1"
   auto_accept          = true
 }
