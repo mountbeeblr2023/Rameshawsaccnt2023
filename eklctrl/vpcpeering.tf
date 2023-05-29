26d45ee86e4169069cce58152b565d070f054bde
ramesh22blr <blr22ramesh@gmail.com>
2023-05-29 21:03:37 +0530

    wrker node

--- eklctrl/vpcpeering.tf
+++ eklctrl/vpcpeering.tf
@@ -6,19 +6,3 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_region          = "us-east-1"
   auto_accept          = true
 }
-
-# VPC 1 Configuration
-
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.16.0.0/20"
-
-  # Add other VPC configuration here (e.g., tags, DNS support, etc.)
-}
-
-# VPC 2 Configuration
-
-resource "aws_vpc" "worker_vpc" {
-  cidr_block = "10.17.0.0/24"
-
-  # Add other VPC configuration here (e.g., tags, DNS support, etc.)
-}
