8bc2c7e4072d6e99d3109b79ca2aeb4525502649
ramesh22blr <blr22ramesh@gmail.com>
2023-05-29 21:19:38 +0530

    wrker node

--- eklctrl/vpcpeering.tf
+++ eklctrl/vpcpeering.tf
@@ -5,3 +5,15 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id          = aws_vpc.worker_vpc.id
   auto_accept          = true
 }
+
+resource "aws_route" "rt_vpc1_peering" {
+  route_table_id            = aws_route_table.eks_route_table.id
+  destination_cidr_block    = aws_vpc.worker_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_route" "rt_vpc2_peering" {
+  route_table_id            = aws_route_table.worker_route_table.id
+  destination_cidr_block    = aws_vpc.eks_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
