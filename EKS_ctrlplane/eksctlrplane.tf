d4516df4059b1a6893d1352fbd0e7e079b77adf5
ramesh22blr <blr22ramesh@gmail.com>
2023-05-30 11:18:44 +0530

    eks ctrlplane

--- EKS_ctrlplane/eksctlrplane.tf
+++ EKS_ctrlplane/eksctlrplane.tf
@@ -1,7 +1,7 @@
 # Create the EKS cluster
 resource "aws_eks_cluster" "eks_ctrlplane01" {
   name     = "eks-clusterctrlplane01"
-  role_arn = "arn:aws:iam::301770107409:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS"
+  role_arn = "arn:aws:iam::301770107409:role/EKS-ctrl-plane-role01"
   version  = "1.24"  # Replace with the desired EKS version
   vpc_config {
     subnet_ids              = module.eks_vpc.EKS_subnet_ids
