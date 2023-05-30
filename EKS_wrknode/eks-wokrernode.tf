29b3deffcb121ec754cae5d916e96be1a7d294af
ramesh22blr <blr22ramesh@gmail.com>
2023-05-30 15:33:11 +0530

    eks worknode

--- EKS_wrknode/eks-wokrernode.tf
+++ EKS_wrknode/eks-wokrernode.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "wrknode_launch_config" {
   image_id      = "ami-1234567890abcdef0"  # Specify the appropriate AMI ID
   instance_type = "t2.micro"     # Specify the desired instance type
   iam_instance_profile = "aws_iam_instance_profile.my_instance_profile.name"
-  security_groups      = [module.worker_vpc.workernode_security_group_ids]
+  security_groups      = [module.eks_vpc.workernode_security_group_ids]
 
   # Add other necessary configuration
 }
@@ -14,13 +14,13 @@ resource "aws_autoscaling_group" "eks_wrk_asg01" {
   max_size                  = 3
   desired_capacity          = 2
   launch_configuration      = aws_launch_configuration.wrknode_launch_config.name
-  vpc_zone_identifier       = module.worker_vpc.workernode_subnet_ids  # Specify the subnet IDs in which the worker nodes should be placed
+  vpc_zone_identifier       = module.eks_vpc.workernode_subnet_ids  # Specify the subnet IDs in which the worker nodes should be placed
 
   # Add other necessary configuration
 }
 
 # Define the VPC module
-module "worker_vpc" {
+module "eks_vpc" {
   source = "../EKS_VPC/"  # Replace with the path to your VPC module folder
   # Additional input variables required by your VPC module
 }
