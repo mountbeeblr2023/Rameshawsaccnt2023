fca1c372e619cb451ddccdad55725affbeec8784
ramesh22blr <blr22ramesh@gmail.com>
2023-05-31 13:35:24 +0530

    eks worknode

--- EKS_Cluster2023/EKS_Cluster.tf
+++ EKS_Cluster2023/EKS_Cluster.tf
@@ -213,20 +213,6 @@ locals {
 }
 
 
-# Retrieve VPC component values using locals
-data "aws_vpc" "existing_vpc" {
-  id = local.vpc_id
-}
-
-data "aws_subnet" "existing_subnet" {
-  for_each = toset(local.subnet_ids)
-  id       = each.value
-}
-
-data "aws_security_group" "existing_security_group" {
-  id = local.security_group_id
-}
-
 # Create EKS cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "my-eks-cluster"
@@ -251,61 +237,6 @@ resource "aws_eks_cluster" "eks_cluster" {
     Environment = "Demo"
   }
 }
-
-# Define local variables to capture workernode VPC component IDs
-locals {
-  worker_vpc_id            = aws_vpc.worker_vpc.id
-  worker_subnet_ids        = [aws_subnet.worker_subnet_a.id, aws_subnet.worker_subnet_b.id]
-  worker_security_group_id = aws_security_group.worker-security-group.id
-}
-# Define local variables to capture EKS VPC component IDs
-locals {
-  vpc_id            = aws_vpc.eks_vpc.id
-  subnet_ids        = {
-    "eks_subnet_a" = aws_subnet.eks_subnet_a.id,
-    "eks_subnet_b" = aws_subnet.eks_subnet_b.id
-  }
-  security_group_id = aws_security_group.eks_cluster_sg.id
-  nacl_id           = aws_network_acl.eks_nacl.id
-}
-
-# ...
-
-# Retrieve VPC component values using locals
-data "aws_vpc" "existing_vpc" {
-  id = local.vpc_id
-}
-
-data "aws_subnet" "existing_subnet" {
-  for_each = local.subnet_ids
-  id       = each.value
-}
-
-# ...
-
-# Define local variables to capture worker node VPC component IDs
-locals {
-  worker_vpc_id            = aws_vpc.worker_vpc.id
-  worker_subnet_ids        = {
-    "worker_subnet_a" = aws_subnet.worker_subnet_a.id,
-    "worker_subnet_b" = aws_subnet.worker_subnet_b.id
-  }
-  worker_security_group_id = aws_security_group.worker-security-group.id
-}
-
-# ...
-
-# Retrieve worker node VPC component values using locals
-data "aws_vpc" "worker_existing_vpc" {
-  id = local.worker_vpc_id
-}
-
-data "aws_subnet" "worker_existing_subnet" {
-  for_each = local.worker_subnet_ids
-  id       = each.value
-}
-
-
 
 # Define the autoscaling group for the worker nodes
 resource "aws_launch_template" "my_worker_node_template" {
@@ -371,8 +302,4 @@ resource "aws_eks_node_group" "my_worker_node_group" {
   capacity_type              = "ON_DEMAND"
 }
 
-# Define the data block to fetch the IAM role ARN
-data "aws_iam_role" "EKS-worker-node-role01" {
-  name = "EKS-worker-node-role01" # Replace with the actual IAM role name
-}
 
---
+++ EKS_Cluster2023/provider.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"  # Set your desired AWS region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    eks = {
+      source  = "hashicorp/aws"
+      version = "~> 0.28"
+    }
+  }
+}
