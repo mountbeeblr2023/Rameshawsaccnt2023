58e8570c9348966126dad15f0c0ea36be2bbd724
ramesh22blr <blr22ramesh@gmail.com>
2023-06-08 19:35:51 +0530

    test01

---
+++ terraform-course/miniproject01/variable.tf
@@ -0,0 +1,11 @@
+variable "aminame" {
+  type        = string
+  default     = "ami-0715c1897453cabd1"
+  description = "useast region ami"
+}
+
+variable myinstancetype {
+  type        = string
+  default     = "t2.micro"
+  description = "micro instance only"
+}
