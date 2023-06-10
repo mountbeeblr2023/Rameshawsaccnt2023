02f0f3329fc92f057716cce0ab991fafb47ac4f3
ramesh22blr <blr22ramesh@gmail.com>
2023-06-10 23:18:46 +0530

    test01

--- terraform-course/miniproject01/variable.tf
+++ terraform-course/miniproject01/variable.tf
@@ -56,4 +56,17 @@ variable blr_public_az {
   description = "AZ for public subnet"
 }
 
+#### variables for private Security Group #######
+variable private_sec_ingress_port {
+  type        = list(number)
+  default     = [80, 443, 8080, 22]
+  description = "list of ingress port"
+}
+
+variable public_sec_ingress_port {
+  type        = list(number)
+  default     = [80, 443, 8080, 22]
+  description = "list of ingress port"
+}
+
 
--- terraform-course/miniproject01/vpc.tf
+++ terraform-course/miniproject01/vpc.tf
@@ -35,38 +35,12 @@ resource "aws_subnet" "project01_private_subnet" {
   }
 }
 
-# ######### Associate the IGW to Public Route Table ############
-# resource "aws_route_table_association" "project01-IGW_association" {
-#   subnet_id         = aws_subnet.project01_subnet_c.id
-#   route_table_id    = aws_route_table.project01_publice_route_table.id
-# }
-
-# ######### Create security group #########
-# resource "aws_security_group" "project01r_sg" {
-#   vpc_id      = aws_vpc.project01_vpc.id
-#   name        = "project01-sg"
-#   description = "Security group for project01"
-
-# ######## Ingress rules ########
-#   ingress {
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-# ####### Egress rules ########
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "project01-sg"
-#   }
-# }
+ ######### Associate the IGW to Public Route Table ############
+ resource "aws_route_table_association" "project01-IGW_association" {
+   route_table_id         = aws_route_table.project01_public_route_table.id
+   destination_cidr_block = "0.0.0.0/0"
+   gateway_id             = aws_internet_gateway.project01-IGW.id
+}
 
 ####### Create private route table for subnets #######
 resource "aws_route_table" "project01_private_route_table" {
@@ -84,7 +58,7 @@ resource "aws_route_table" "project01_public_route_table" {
   }
 }
 
-####### subnet associations with route table & Nacls  ########
+####### subnet associations with route table ########
 resource "aws_route_table_association" "project01_private_route_table_association" {
   count          = length(aws_subnet.project01_private_subnet)
   subnet_id      = aws_subnet.project01_private_subnet[count.index].id
@@ -95,23 +69,4 @@ resource "aws_route_table_association" "project01_public_route_table_association
   subnet_id      = aws_subnet.project01_public_subnet.id
   route_table_id = aws_route_table.project01_public_route_table.id
 }
-
-# resource "aws_network_acl" "project01_nacl" {
-#   vpc_id = aws_vpc.project01_vpc.id
-#   subnet_ids = [aws_subnet.project01_subnet_a.id, aws_subnet.project01_subnet_b.id, aws_subnet.project01_subnet_c.id]
-#   tags = {
-#     Name = "project01-nacl"
-#   }
-# }
-##### Output #######
-# output "project01_subnet_ids" {
-#   value = [aws_subnet.project01_subnet_a.id, aws_subnet.project01_subnet_b.id]
-# }
-# output "project01_vpc_id" {
-#   value = aws_vpc.project01_vpc.id
-# }
-
-# output "project01_security_group_ids" {
-#   value = [aws_security_group.project01_sg.id]
-# }
 
