8238308a3d58758d375c1f47f8ba6ea0efcc0b3c
ramesh22blr <blr22ramesh@gmail.com>
2023-06-10 07:20:55 +0530

    test01

--- terraform-course/miniproject01/ec2-instance.tf
+++ terraform-course/miniproject01/ec2-instance.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "testserver01" {
-  ami           = "ami-0715c1897453cabd1"  # Update with your desired AMI ID
-  instance_type = "t2.micro"                # Update with your desired instance type
+  ami           = var.aminame  # Update with your desired AMI ID
+  instance_type = lookup(var.multi-ami, aws_instance.testserver01.tags["RedHat"])               # Update with your desired instance type
 
   tags = {
     app = "meemo"
     tier = "dev"
+    os = "RedHat"
   }
 }
+
