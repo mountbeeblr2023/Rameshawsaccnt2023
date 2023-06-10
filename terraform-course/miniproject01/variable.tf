1b57462edc3368926340966f31abc3b6ad186228
ramesh22blr <blr22ramesh@gmail.com>
2023-06-10 07:39:08 +0530

    test01

--- terraform-course/miniproject01/variable.tf
+++ terraform-course/miniproject01/variable.tf
@@ -1,7 +1,17 @@
-variable "aminame" {
-  type        = string
-  default     = "ami-0715c1897453cabd1"
-  description = "useast region ami"
+# variable "aminame" {
+#   type        = string
+#   default     = "ami-0715c1897453cabd1"
+#   description = "useast region ami"
+# }
+
+variable multi-ami {
+  type        = map
+  default     = {
+    Amazon-ami = "ami-04a0ae173da5807d3"
+    RedHat-ami = "ami-026ebd4cfe2c043b2"
+    Ubuntu-ami = "ami-053b0d53c279acc90"
+  }
+  description = "description"
 }
 
 variable myinstancetype {
@@ -9,3 +19,5 @@ variable myinstancetype {
   default     = "t2.micro"
   description = "micro instance only"
 }
+
+
