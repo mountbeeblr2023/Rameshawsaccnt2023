4acbac012f44b8ed3f461ed5672df9eb8cbef4e3
ramesh22blr <blr22ramesh@gmail.com>
2023-06-10 11:01:04 +0530

    test01

--- terraform-course/miniproject01/ec2-instance.tf
+++ terraform-course/miniproject01/ec2-instance.tf
@@ -1,16 +1,35 @@
-resource "aws_instance" "testserver01" {
-  ami           = local.selected_ami
-  instance_type = var.myinstancetype
+variable "instances" {
+  type = list(object({
+    name           = string
+    ami_id         = string
+    instance_type  = string
+  }))
+  default = [
+    {
+      name           = "dev"
+      ami_id         = "ami-04a0ae173da5807d3"  # Replace with your dev AMI ID
+      instance_type  = "t2.micro"     # Replace with your dev instance type
+    },
+    {
+      name           = "qa"
+      ami_id         = "ami-026ebd4cfe2c043b2"  # Replace with your qa AMI ID
+      instance_type  = "t2.medium"    # Replace with your qa instance type
+    },
+    {
+      name           = "prod"
+      ami_id         = "ami-053b0d53c279acc90"  # Replace with your prod AMI ID
+      instance_type  = "t2.large"     # Replace with your prod instance type
+    }
+  ]
+}
+
+resource "aws_instance" "ec2_instances" {
+  count = length(var.instances)
+
+  ami           = var.instances[count.index].ami_id
+  instance_type = var.instances[count.index].instance_type
 
   tags = {
-    app = "meemo"
-    tier = "dev"
-    os = "RedHat-ami"
-    Name = "testserver01"
+    Name = var.instances[count.index].name
   }
 }
- locals {
-   #selected_ami = lookup(var.multi-ami, "RedHat-ami", "default-ami-id")
-   selected_ami = lookup(var.multi_ami, aws_instance.testserver01.tags["os"], "default-ami-id")
-
-    }
