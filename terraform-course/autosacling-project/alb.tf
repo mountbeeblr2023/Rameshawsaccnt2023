ebe339128b54fc5d202eb473307be111e1367be7
ramesh22blr <blr22ramesh@gmail.com>
2023-06-11 22:48:53 +0530

    autotest001

--- terraform-course/autosacling-project/alb.tf
+++ terraform-course/autosacling-project/alb.tf
@@ -2,8 +2,9 @@ resource "aws_lb" "blr-public-alb" {
   name               = "blr-public-alb"
   internal           = false
   load_balancer_type = "application"           
-  count              = length(aws_subnet.project01_public_subnet)
-  subnets            = [aws_subnet.project01_public_subnet[count.index].id]
+ #count              = length(aws_subnet.project01_public_subnet)
+ #subnets            = [aws_subnet.project01_public_subnet[count.index].id]
+  subnets           = [aws_subnet.project01_public_subnet[0].id, aws_subnet.project01_public_subnet[1].id]
   security_groups    = [aws_security_group.project01_public_secgroup01.id]
 
   tags = {
