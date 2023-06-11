9dfa9660209605c49bf3c02c2f2e6bbe882551ff
ramesh22blr <blr22ramesh@gmail.com>
2023-06-11 12:23:37 +0530

    test01

--- terraform-course/miniproject01/ec2-instance.tf
+++ terraform-course/miniproject01/ec2-instance.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "ec2_instances" {
   count = length(var.instances)
 
   ami                         = var.instances[count.index].ami_id
-  instance_type               = var.instances[count.index].instance_typesubnet_id = aws_subnet.project01_private_subnet[count.index].id  
+  instance_type               = var.instances[count.index].instance_type
+  subnet_id = aws_subnet.project01_private_subnet[count.index].id  
   vpc_security_group_ids      = [aws_security_group.project01_private_secgroup01[count.index].id]  
   associate_public_ip_address = false 
 
