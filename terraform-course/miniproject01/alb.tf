a543ea37abdb2a50ad2160ad366ef21f538aae01
ramesh22blr <blr22ramesh@gmail.com>
2023-06-11 12:56:03 +0530

    test01

--- terraform-course/miniproject01/alb.tf
+++ terraform-course/miniproject01/alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "project01_public_alb" {
   name               = "project01_public_alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [project01_public_subnet.id]
+  subnets            = [aws_subnet.project01_public_subnet.id]
 
   security_groups    = [aws_security_group.project01_public_secgroup01.id]
 
@@ -10,8 +10,8 @@ resource "aws_lb" "project01_public_alb" {
     Name = "project01_public_alb"
   }
 }
-resource "aws_lb_target_group" "blr_alb_target_group" {
-  name        = "blr_alb_target_group"
+resource "aws_lb_target_group" "blr-alb-target-group" {
+  name        = "blr-alb-target-group"
   port        = 443
   protocol    = "HTTPS"
   vpc_id      = aws_vpc.project01_vpc.id
@@ -19,7 +19,7 @@ resource "aws_lb_target_group" "blr_alb_target_group" {
 
 resource "aws_lb_target_group_attachment" "ec2_target_attachment" {
   count           = length(var.instances)
-  target_group_arn = aws_lb_target_group.blr_alb_target_group.arn
+  target_group_arn = aws_lb_target_group.blr-alb-target-group.arn
   target_id       = aws_instance.ec2_instances[count.index].id
   port            = 443
 }
