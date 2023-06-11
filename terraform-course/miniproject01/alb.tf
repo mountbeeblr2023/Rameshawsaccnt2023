f7fea89f623c26dcbb8ebb4771001d3df40167cd
ramesh22blr <blr22ramesh@gmail.com>
2023-06-11 12:47:40 +0530

    test01

---
+++ terraform-course/miniproject01/alb.tf
@@ -0,0 +1,25 @@
+resource "aws_lb" "project01_public_alb" {
+  name               = "project01_public_alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [project01_public_subnet.id]
+
+  security_groups    = [aws_security_group.project01_public_secgroup01.id]
+
+  tags = {
+    Name = "project01_public_alb"
+  }
+}
+resource "aws_lb_target_group" "project01_alb_target_group" {
+  name        = "project01_alb_target_group"
+  port        = 443
+  protocol    = "HTTPS"
+  vpc_id      = aws_vpc.project01_vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "ec2_target_attachment" {
+  count           = length(var.instances)
+  target_group_arn = aws_lb_target_group.project01_alb_target_group.arn
+  target_id       = aws_instance.ec2_instances[count.index].id
+  port            = 443
+}
