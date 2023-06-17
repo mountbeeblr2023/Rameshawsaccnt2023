956627330fd619a3381e61a909bb4b925101988a
ramesh22blr <blr22ramesh@gmail.com>
2023-06-17 13:30:35 +0530

    test-module-1

--- Iac2023/source-main.tf
+++
@@ -1,9 +0,0 @@
- module "ec2-config" {
-     source = "./modules/ec2resource"   
- }
- module "secgroup-config" {
-     source = "./modules/securitygroups"
- }
-module "vpc-config" {
-    source = "./modules/vpcmodule"
-}
