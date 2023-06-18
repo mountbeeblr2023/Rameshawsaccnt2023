8b63819c905bd6226a36cfc246131fe8ba606758
ramesh22blr <blr22ramesh@gmail.com>
2023-06-18 23:08:25 +0530

    test-project-2

--- terraform-VPCmodules-proj02/VPC-with-multiBlock/main.tf
+++ terraform-VPCmodules-proj02/VPC-with-multiBlock/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
 }
 
 module "subnets" {
-  source         = "./subnetmodule"
+  source         = "./subnetmodule/privatesub"
   vpc_id         = module.vpc.vpc_id
   subnet_configs = [
     {
